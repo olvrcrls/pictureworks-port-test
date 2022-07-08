234447912e0f0ccac509f137b115f211cc1164bb
olvrcrls <dev.olivercarlos@gmail.com>
2022-07-09 00:04:18 +0800

    Add macro for response for success and error responses on API.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use Exception;
 use App\Repositories\UserRepository;
 use App\Repositories\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /** Applying macro for more readable response and programmability */
+        Response::macro('success', function ($data, $message = 'OK') {
+            return response()->json([
+                'data' => $data,
+                'message' => $message
+            ], 200);
+        });
+
+        Response::macro('error', function ($error, $status_code = 422) {
+            if (env('SCRIPT')) throw new Exception($error);
+            Log::error($error);
+
+            return response()->json([
+                'error' => $error,
+            ], $status_code);
+        });
     }
 }
